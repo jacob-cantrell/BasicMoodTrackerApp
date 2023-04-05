1658ead394853ece1abcce9bd103a314432dc6e7
jacob-cantrell <jcantrell218@gmail.com>
2023-04-05 11:56:29 -0400

    fixed centering of navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,22 +19,15 @@ const Header = () => {
             </h1>
 
             <nav className="navbar navbar-expand-lg bg-light">
-                <div className="container-fluid" style={{ marginLeft: "33%" }}>
-                    <Link className="navbar-brand" to="/home" >Home</Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-
-                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <div className="navbar-nav">
-                            <Link className="nav-link" to="/add-journal">Add Journal</Link>
-                            <Link className="nav-link" to="/view-journal">View Journals</Link>
-                            <Link className="nav-link" to="/mood-tracker">Mood Tracker</Link>
-                            <Link className="nav-link" to="/about">About Us</Link>
-                            <Link className="nav-link" to="/contact">Contact</Link>
-                        </div>
+                <div className="container-fluid">
+                    <div className="navbar-nav center">
+                        <Link className="navbar-brand" to="/home" >Home</Link>
+                        <Link className="nav-link" to="/add-journal">Add Journal</Link>
+                        <Link className="nav-link" to="/view-journal">View Journals</Link>
+                        <Link className="nav-link" to="/mood-tracker">Mood Tracker</Link>
+                        <Link className="nav-link" to="/about">About Us</Link>
+                        <Link className="nav-link" to="/contact">Contact</Link>
                     </div>
-
                 </div>
             </nav>
         </div>
