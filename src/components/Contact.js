d29b3fdaadd4ce66096f67a3eebd4a03cc20c7f5
jacob-cantrell <jcantrell218@gmail.com>
2023-04-05 11:29:31 -0400

    added fillers for Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,8 +11,27 @@ const Contact = () => {
                         Contact Page
                 </h2>
             </div>
+            <br></br>
+            <div style={{textAlign: "center"}}>
+                <label style={{textAlign: "center"}}>About this website:</label>
+                <br></br>
+                <p>Sample text.</p>
+            </div>
+            <br></br>
             <div>
+                <table className="styled-table center">
+                    <thead>
+                        <tr>
+                            <th>Resource Name</th>
+                            <th>Website</th>
+                            <th>Email</th>
+                            <th>Phone Number</th>
+                        </tr>
+                    </thead>
+                    <tbody>
 
+                    </tbody>
+                </table>
             </div>
         </div>
     );
